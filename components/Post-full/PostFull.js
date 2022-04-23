99a7d76e08e10f8b40f1c2eb8dc95343aeebcfec
Mancefas <mantvydasliobe@gmail.com>
2022-04-23 20:44:21 +0300

    Added : back button, css changes

--- components/Post-full/PostFull.js
+++ components/Post-full/PostFull.js
@@ -1,6 +1,7 @@
-import { Container } from "@mui/material";
+import { Container, Box, Button } from "@mui/material";
 import styles from "./PostFull.module.css";
 import { marked } from "marked";
+import { Link } from "@mui/material";
 
 const PostFull = ({ content }) => {
   return (
@@ -16,6 +17,14 @@ const PostFull = ({ content }) => {
           className={styles.content}
           dangerouslySetInnerHTML={{ __html: marked(content) }}
         ></div>
+        <Box sx={{ textAlign: "center" }}>
+          <Link href="/" passHref style={{ textDecoration: "none" }}>
+            <Button variant="outlined" color="success">
+              {" "}
+              Atgal
+            </Button>
+          </Link>
+        </Box>
       </Container>
     </>
   );
