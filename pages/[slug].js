4aabdb3a7809e0af6520afde56fc7c7b2c066f0a
Mancefas <mantvydasliobe@gmail.com>
2022-05-09 20:50:16 +0300

    GA4 code with consent added to all pages

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,7 +5,36 @@ import PostFull from "../components/Post-full/PostFull";
 import Layout from "../components/Layout";
 import Head from "next/head";
 
+import CookieConsent, {
+  Cookies,
+  getCookieConsentValue,
+} from "react-cookie-consent";
+import { initGA } from "../utils/GA-utils";
+
+import { useEffect } from "react";
+import Link from "next/link";
+
 const Post = ({ content, frontmatter }) => {
+  const cookieAcepted = () => {
+    initGA(process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ANALYTICS_ID);
+  };
+
+  const cookieDeclined = () => {
+    //remove google analytics cookies
+    Cookies.remove("_ga");
+    Cookies.remove("_gat");
+    Cookies.remove("_gid");
+  };
+
+  useEffect(() => {
+    const isConsent = getCookieConsentValue();
+    if (isConsent === "true") {
+      cookieAcepted();
+    } else if (isConsent === "false") {
+      cookieDeclined;
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -18,6 +47,28 @@ const Post = ({ content, frontmatter }) => {
       </Head>
       <Layout>
         <PostFull content={content} />
+        <CookieConsent
+          buttonText="Supratau, naršau toliau"
+          onAccept={cookieAcepted}
+          enableDeclineButton
+          declineButtonText="neleisti"
+          onDecline={cookieDeclined}
+          flipButtons
+          style={{ opacity: "0.7" }}
+          declineButtonStyle={{
+            background: "#ffffff",
+            color: "black",
+            opacity: "0.5",
+          }}
+          buttonStyle={{ background: "#ffffff", opacity: "1" }}
+        >
+          <p>
+            Šioje svetainėje naudojame slapukus, kad užtikrintume tinkamą
+            svetainės veikimą, analizuotume naršymo statistiką,
+            individualizuotume Jūsų naršymo patirtį bei teiktume Jums aktualius
+            naujus straipsnius. <Link href="/privatumo-politika/">Plačiau</Link>
+          </p>
+        </CookieConsent>
       </Layout>
     </>
   );
